8c8a1a0e471cc44755af18b79593303fa20bca48
Denis_Moiseev <denis_moiseev@epam.com>
2019-11-20 16:03:59 +0300

    EPMDJ-1869. added SecondTest with new 3 tests methods

---
+++ src/test/kotlin/SecondTest.kt
@@ -0,0 +1,31 @@
+import io.restassured.RestAssured.given
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class SecondTest {
+
+    lateinit var petclinicUrl: String
+
+    @BeforeAll
+    fun before() {
+        val propUrl = System.getProperty("petclinic.url", "localhost:8082")
+        petclinicUrl = "http://$propUrl"
+    }
+
+    @Test
+    fun getOwnerFindPage() {
+        given().get("$petclinicUrl/owners/find").then().statusCode(200)
+    }
+
+    @Test
+    fun getHomePage() {
+        given().get("$petclinicUrl/").then().statusCode(200)
+    }
+
+    @Test
+    fun getErrorPage() {
+        given().get("$petclinicUrl/oups").then().statusCode(200)
+    }
+}
