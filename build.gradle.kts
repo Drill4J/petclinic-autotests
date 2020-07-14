f30e8d1d3a33711ef925a16d38d8ab36d017d25e
Denis_Moiseev <denis_moiseev@epam.com>
2020-07-14 08:58:01 +0300

    change autotest agent version

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.3.50"
-    id("com.epam.drill.agent.runner.autotest") version ("0.1.2")
+    id("com.epam.drill.agent.runner.autotest") version ("0.1.4")
 }
 
 group = "com.epam.epm-d4j"
@@ -30,7 +30,7 @@ tasks.withType<KotlinCompile> {
 }
 
 drill {
-    version = "0.1.4"
+    version = "0.2.0"
     adminHost = System.getProperty("adminHost")
     if (System.getProperty("groupId") != null) {
         groupId = System.getProperty("groupId")
