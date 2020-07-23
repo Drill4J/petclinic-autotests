3e6ce320fb7c3691ad80927c3c51dc576c4970e2
Denis_Moiseev <denis_moiseev@epam.com>
2020-07-23 14:14:25 +0300

    update version of autotest agent

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ tasks.withType<KotlinCompile> {
 }
 
 drill {
-    version = "0.3.0"
+    version = "0.3.3"
     adminHost = System.getProperty("adminHost")
     if (System.getProperty("groupId") != null) {
         groupId = System.getProperty("groupId")
