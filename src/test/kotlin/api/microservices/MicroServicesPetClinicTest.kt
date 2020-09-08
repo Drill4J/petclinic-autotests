c919301e746c671eb1f1c4ba82323f3112811557
Denis_Moiseev <denis_moiseev@epam.com>
2020-09-08 12:04:12 +0300

    add service waiter for mcr petclinic tests

--- src/test/kotlin/api/microservices/MicroServicesPetClinicTest.kt
+++ src/test/kotlin/api/microservices/MicroServicesPetClinicTest.kt
@@ -4,6 +4,8 @@ import io.restassured.RestAssured.given
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
+import java.lang.Thread.sleep
+import java.net.ConnectException
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class MicroServicesPetClinicTest {
@@ -13,6 +15,23 @@ class MicroServicesPetClinicTest {
     @BeforeAll
     fun before() {
         petclinicUrl = System.getProperty("petclinicUrl", "http://localhost:8082")
+        val urls = arrayListOf(
+            "$petclinicUrl/api/visit/owners/2/pets/2/visits",
+            "$petclinicUrl/api/customer/owners",
+            "$petclinicUrl/api/vet/vets"
+        )
+        var isMcrPetReadyForTests = false
+        while (!isMcrPetReadyForTests) {
+            urls.forEach {
+                isMcrPetReadyForTests = try {
+                    given().`when`().get(it).then().extract().response().statusCode == 200
+                } catch (e: ConnectException) {
+                    false
+                }
+                sleep(3000)
+                print("waiting for services...")
+            }
+        }
     }
 
     @Test
@@ -31,11 +50,15 @@ class MicroServicesPetClinicTest {
             .body("{\"id\":1,\"firstName\":\"George\",\"lastName\":\"Franklin\",\"address\":\"110 W. Liberty St.\",\"city\":\"Autotest\",\"telephone\":\"6085551023\",\"pets\":[{\"id\":1,\"name\":\"Leo\",\"birthDate\":\"2010-09-07\",\"type\":{\"id\":1,\"name\":\"cat\"}}]}")
             .`when`()
             .put("$petclinicUrl/api/customer/owners/1")
+            .then()
+            .statusCode(200)
     }
 
     @Test
     fun getVetsInfoTest() {
         given()
             .get("$petclinicUrl/api/vet/vets")
+            .then()
+            .statusCode(200)
     }
 }
