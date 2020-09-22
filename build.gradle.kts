6ec81064e73c8ae638de2bae47184f8133e722bb
Denis_Moiseev <denis_moiseev@epam.com>
2020-09-22 13:17:53 +0300

    autotest agent up to 0.8.2 version

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ tasks.withType<KotlinCompile> {
 }
 
 drill {
-    version = "0.7.0"
+    version = "0.8.2"
     adminHost = System.getProperty("adminHost")
     if (System.getProperty("groupId") != null) {
         groupId = System.getProperty("groupId")
