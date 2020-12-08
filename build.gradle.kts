8a001263117a218bfa3745a1f0e32c34f114f150
Denis_Moiseev <denis_moiseev@epam.com>
2020-12-08 10:27:55 +0300

    update agent version to 0.10.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ tasks.withType<KotlinCompile> {
 }
 
 drill {
-    version = "0.9.1"
+    version = "0.10.2"
     adminHost = System.getProperty("adminHost")
     if (System.getProperty("groupId") != null) {
         groupId = System.getProperty("groupId")
